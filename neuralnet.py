52017c8710a3b646a94adb33f7bb12390517968c
Yahor Mankevich <mankeviczjegor@gmail.com>
2025-03-01 00:20:00 +0100

    Add Neuron class

--- neuralnet.py
+++ neuralnet.py
@@ -12,8 +12,16 @@ class Base:
 class Neuron(Base):
     def __init__(self, inputs_num, nonlin = True):
         self.weight = [Tensor(random.uniform(-1,1)) for _ in range(inputs_num)]
-        self.bias = 0
+        self.bias = Tensor(0)
         self.nonlin = nonlin
 
     def __call__(self, input_vec):
-        act = sum()
+        act = sum((wi * xi for wi,xi in zip(self.weight,input_vec)), self.bias)
+        return act.relu() if self.nonlin else act
+
+    def parameters(self):
+        return self.weight + [self.bias]
+        
+    def __repr__(self):
+        return f"{'Relu' if self.nonlin else 'Linear'}Neuron({len(self.weight)})"
+    
